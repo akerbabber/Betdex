148911888e4015a355d5fbaedb93159680097dd1
akerbabber <maurizio.murru93@gmail.com>
2020-10-26 16:48:27 +0000

    resolveMarket() function added
    resolveMarket() calls reportPayouts in the ConditionalTokens contract feeding the payout.

--- packages/buidler/contracts/GasOracle.sol
+++ packages/buidler/contracts/GasOracle.sol
@@ -2,17 +2,36 @@ pragma solidity ^0.5.0;
 
 import "@chainlink/contracts/src/v0.5/ChainlinkClient.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "@gnosis.pm/conditional-tokens-contracts/contracts/ConditionalTokens.sol";
 
 contract GasOracle is ChainlinkClient, Ownable {
   
+  uint256 constant PAYMENT = 1 * LINK;
   uint256 oraclePayment;
   int256 public gasPrice;
+  address public oracle;
   
-  constructor(uint256 _oraclePayment) public {
+  constructor(address _oracle) public {
     setPublicChainlinkToken();
-    oraclePayment = _oraclePayment;
+    oraclePayment = PAYMENT;
+    oracle = _oracle;
   }
   // Additional functions here:
+
+  function resolveMarket (address addr, bytes32 questionId) public onlyOwner {
+    ConditionalTokens c = ConditionalTokens(addr);
+    uint[] memory payouts;
+    if (gasPrice>20) {
+       payouts[0] = 1;
+       payouts[1] = 0;
+    }
+    else {
+      payouts[0] = 0;
+      payouts[1] = 1;
+    }
+        return c.reportPayouts(questionId, payouts);
+  }
+
   function requestGasPriceByDate(address _oracle, bytes32 _jobId, string memory _date)
   public
   onlyOwner
@@ -27,8 +46,10 @@ contract GasOracle is ChainlinkClient, Ownable {
 
 
 
+
+
 function fulfillGasPrice(bytes32 _requestId, int256 _gasPrice)
-  public
+  public onlyOwner
   recordChainlinkFulfillment(_requestId)
 {
   gasPrice = _gasPrice;
